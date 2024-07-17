275d94c66b41c7c6c83749c9c2891df58f60257b
guohan <poethan@foxmail.com>
2024-07-17 14:28:58 +0800

    feat: add new func for number class

--- core/native/basic/EzInteger.php
+++ core/native/basic/EzInteger.php
@@ -1,4 +1,41 @@
 <?php
-final class EzInteger{
+final class EzInteger extends EzNumber {
+    private $value;
 
+    public function __construct($value)
+    {
+        $this->value = intval($value);
+    }
+
+    public function longValue()
+    {
+        return $this->value;
+    }
+
+    public function intValue()
+    {
+        return $this->value;
+    }
+
+    public function floatValue()
+    {
+        return floatval($this->value);
+    }
+
+    public function doubleValue()
+    {
+        return doubleval($this->value);
+    }
+
+    public function byteValue()
+    {
+        return 0;
+    }
+
+    public function equals($object) {
+        if ($object instanceof EzInteger) {
+            return $this->value === $object->intValue();
+        }
+        return false;
+    }
 }
--- core/native/basic/EzNumber.php
+++ core/native/basic/EzNumber.php
@@ -1,5 +1,9 @@
 <?php
-abstract class EzNumber
+abstract class EzNumber extends EzObject
 {
-    
+    public abstract function longValue();
+    public abstract function intValue();
+    public abstract function floatValue();
+    public abstract function doubleValue();
+    public abstract function byteValue();
 }
