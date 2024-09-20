1f2833d70fd86be0bb0b153e66a7efe03b5dc948
guohan <poethan@foxmail.com>
2024-09-20 15:10:08 +0800

    feat: add bin for env check

--- bin/check_env.sh
+++ bin/check_env.sh
@@ -10,35 +10,6 @@ source "$SCRIPT_DIR/repository/language.sh"
 # 输出脚本版本
 print_info "$VERSION_MSG"
 
-# 检查 PHP 是否安装
-if ! command -v php &> /dev/null; then
-    print_error "$PHP_NOT_INSTALLED_MSG"
-    exit 1
-fi
-
-# 获取 PHP 版本
-PHP_VERSION=$(php -r "echo PHP_VERSION;")
-if [[ $(echo "$PHP_VERSION 7.4" | awk '{print ($1 > $2)}') -eq 1 ]]; then
-    print_warning "$PHP_VERSION_WARNING_MSG"
-fi
-if [[ $(echo "$PHP_VERSION 8.1" | awk '{print ($1 > $2)}') -eq 1 ]]; then
-    print_success "$PHP_VERSION_SUCCESS_MSG"
-fi
-
-# 检查 PHP 必须组件
-MISSING_COMPONENTS=()
-for component in curl pdo mysqli openssl sockets json pdo_mysql libxml mbstring; do
-    if ! php -m | grep -q "$component"; then
-        MISSING_COMPONENTS+=("$component")
-    fi
-done
-
-if [ ${#MISSING_COMPONENTS[@]} -eq 0 ]; then
-    print_success "$PHP_COMPONENTS_SUCCESS_MSG"
-else
-    print_error "$PHP_COMPONENTS_ERROR_MSG ${MISSING_COMPONENTS[*]}"
-fi
-
 # 检测操作系统
 if [ -f /etc/os-release ]; then
     . /etc/os-release
@@ -62,3 +33,36 @@ case "$OS" in
         print_warning "Unsupported or unrecognized operating system"
         ;;
 esac
+
+# 检查 PHP 是否安装
+if ! command -v php &> /dev/null; then
+    print_error "$PHP_NOT_INSTALLED_MSG"
+    exit 1
+fi
+
+# 获取 PHP 版本
+PHP_VERSION=$(php -r "echo PHP_VERSION;")
+
+# 使用 sort -V 进行版本比较
+if [[ "$(printf '%s\n' "7.4" "$PHP_VERSION" | sort -V | head -n1)" == "7.4" && "$PHP_VERSION" != "7.4" ]]; then
+    print_warning "$PHP_VERSION_WARNING_MSG"
+fi
+
+if [[ "$(printf '%s\n' "8.1" "$PHP_VERSION" | sort -V | head -n1)" == "8.1" && "$PHP_VERSION" != "8.1" ]]; then
+    print_success "$PHP_VERSION_SUCCESS_MSG"
+fi
+
+# 检查 PHP 必须组件
+MISSING_COMPONENTS=()
+for component in curl pdo mysqli openssl sockets json pdo_mysql libxml mbstring; do
+    if ! php -m | grep -q "$component"; then
+        MISSING_COMPONENTS+=("$component")
+    fi
+done
+
+if [ ${#MISSING_COMPONENTS[@]} -eq 0 ]; then
+    print_success "$PHP_COMPONENTS_SUCCESS_MSG"
+else
+    print_error "$PHP_COMPONENTS_ERROR_MSG ${MISSING_COMPONENTS[*]}"
+fi
+
